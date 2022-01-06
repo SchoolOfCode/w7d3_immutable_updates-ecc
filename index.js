e23ed000f662455e2d488b5ab9280aecd3118044
ciisiq <cii_3@hotmail.com>
2022-01-06 12:36:52 +0000

    Q3 done

--- index.js
+++ index.js
@@ -2,27 +2,34 @@
 
 // Immutably add an item to the end of an array
 export function addToEnd(array, item) {
-    const newArray = [...array];
-    newArray.push(item);
-    return newArray;
-    //push item to end of spread array
-    //
+  const newArray = [...array];
+  newArray.push(item);
+  return newArray;
+  //push item to end of spread array
+  //
 }
 
 // Immutably add an item to the beginning of an array
 export function addToBeginning(array, item) {
-    const newArray = [...array];
-    newArray.unshift(item);
-    return newArray;
+  const newArray = [...array];
+  newArray.unshift(item);
+  return newArray;
 }
 //function takes in item and array
 //spread array
 //add item to start of array
 //return new array
 
-
 // Immutably insert an item at a specific position/index within an array
-export function insertItem(array, item, index) {}
+export function insertItem(array, item, index) {
+  //spread the array
+  //add new Item to the array
+  // needs to go to specific index/position
+  // return the new array
+  const newArray = [...array.slice(0, index), item, ...array.slice(index)];
+  //   newArray.slice(item);
+  return newArray;
+}
 
 // Immutably replace an item at a specific position/index within an array
 export function replaceItem(array, item, index) {}
